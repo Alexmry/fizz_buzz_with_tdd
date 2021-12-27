715ec2d5af3ea11dd381a28c6df2c13d26e8797d
Alexandre Morey <alexander006_9@hotmail.com>
2021-12-27 11:54:52 +0100

    wrote the first test for the numbers from 1 to 100

--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -1,7 +1,13 @@
 require "fizz_buzz"
 
 RSpec.describe FizzBuzz do
-    it "outputs the number from 1 to 100"
+    subject(:outputs) { described_class.outputs }
+    it "outputs the number from 1 to 100" do
+        expect(outputs).to eq(('1'..'100')to_a)
+        expect(outputs.first).to eq(1)
+        expect(outputs.last).to eq(100)
+    end
+
     it "outputs Fizz for multiples of 3"
     it "outputs Buzz for multiples of 5"
     it "outputs FizzBuzz for multiples of 3 and 5"
